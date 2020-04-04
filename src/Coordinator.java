ab453fe13970d7c0844a32edc63660d0434b41ea
Dave <dew1g18@soton.ac.uk>
2020-04-04 01:29:11 +0000

    the Coordinator has register functions etc, run is ready to be made but I'm moving over to look at Participant for a little while as I think I've been creating framework for the thing that part one is asking for to use, not making the actual thing... I really am not sure what this course is asking us to do tbh smh lol very tired.. blame corona..

--- src/Coordinator.java
+++ src/Coordinator.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Coordinator {
 
@@ -7,22 +10,35 @@ public class Coordinator {
      * First I'll need some variables that the coordinator will need to keep track of, like the chat server
      *
      * These will include:
+     *      -Min connections (the spec wants the coordinator to wait for a certain number of processes to join.
      *      -Max Connections (given in spec as N connections)
      *      -Current number of connections, used to keep track of whether we've reached the max number
      *      -A collection of all the connections active, will use a map of names to sockets.
      */
+    private int minPorts = 10;
+    //todo check if there is to be a max ports implemented, the spec as far as I've read only has a lower bound.
+
+    private int numberOfConnections = 0;
+
+    private Map<String, PrintWriter> stored = Collections.synchronizedMap(new HashMap<String, PrintWriter>());
+    //todo: Above in the final set of parenthesis should be the maxPorts variable if one is to be implemented.
+
+
+
+
+
 
     private class ServerThread extends Thread{
-        private Socket clientSocket;
-        private String clientName;
-        private BufferedReader clientIn;
-        private PrintWriter clientOut;
+        private Socket portSocket;
+        private String portName;
+        private BufferedReader portIn;
+        private PrintWriter portOut;
 
-        ServerThread(Socket client) throws IOException {
-            clientSocket= client;
+        ServerThread(Socket port) throws IOException {
+            portSocket= port;
 
-            clientIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            clientOut = new PrintWriter(new OutputStreamWriter(client.getOutputStream()));
+            portIn = new BufferedReader(new InputStreamReader(port.getInputStream()));
+            portOut = new PrintWriter(new OutputStreamWriter(port.getOutputStream()));
 
         }
 
@@ -32,8 +48,44 @@ public class Coordinator {
          */
 
         public void run(){
+            TokenHandler tokenHandler = new TokenHandler();
+            try {
+                Token token = tokenHandler.getToken(portIn.readLine());
 
+            }catch(IOException e){
+                e.printStackTrace();
+
+            }
         }
     }
+
+    /**
+     * The following method attempts to register a new port with the coordinator, and returns a boolean
+     * @return success?
+     */
+    boolean register(String port, PrintWriter out){
+        //todo ??May need to apply a maxPorts connected bound, but for now the spec doesn't seem to have such a requirement.
+        if (stored.containsKey((port))){
+            System.err.println("Port already registered, what are you trying to pull?");
+            return false;
+        }
+        try{
+            stored.put(port, out);
+        }catch(NullPointerException e) {
+            System.out.println("THE NULL POINTER YOU WERENT EXPECTING THAT WAS BROUGHT FROM THE CHAT SERVER WAS TRIPPED, PLEASE INSPECT!!!");
+            return false;
+        }
+        this.numberOfConnections++;
+        return true;
+    }
+
+    //Just to ensure that the map and number of connections stay updated in the event of a process becoming unregistered.
+    void unregister(String port){
+        stored.remove(port);
+        this.numberOfConnections--;
+
+    }
+
+
 
 }
