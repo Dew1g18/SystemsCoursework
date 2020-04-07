d39baf88b2afc3ced2f166b9dfc1914a09f738c7
Dave <dew1g18@soton.ac.uk>
2020-04-07 00:55:28 +0000

    really simple logic and method implemented now in VotingThread, it will take the options (DetailToken) from the participant and a voteToken to send to each of the options. EZ

--- src/VotingThread.java
+++ src/VotingThread.java
@@ -1,2 +1,38 @@
-public class VotingThread {
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class VotingThread extends Thread {
+
+//    private Socket mySocket;
+    private VoteToken voteToken;
+    private boolean finishedVoting;
+    private DetailToken details;
+
+    public VotingThread(VoteToken voteToken, DetailToken detailToken){
+//        this.mySocket = socket;
+        this.voteToken = voteToken;
+        this.details = detailToken;
+        finishedVoting = false;
+    }
+
+    public void run(){
+        Socket socket;
+        try {
+            for (String port : details.getOptions()) {
+                socket = new Socket(InetAddress.getLocalHost(), Integer.parseInt(port));
+                PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+                out.println(voteToken.requirement);
+                out.flush();
+            }
+            finishedVoting=true;
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
